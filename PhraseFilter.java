106fb4d2a4cfcc7cf49eb97f2dfc54672a6d8fd6
John Buchanan Moore <johnbucmoore@gmail.com>
2019-12-18 14:45:16 -0500

    add PhraseFilter class

---
+++ PhraseFilter.java
@@ -0,0 +1,20 @@
+public class PhraseFilter {
+    private String where;
+    private String phrase;
+    public PhraseFilter(String w, String p) {
+        where = w;
+        phrase = p;
+    }
+    public boolean satisfies(QuakeEntry qe) {
+        String title = qe.getInfo();
+        if (where.equals("any") && title.contains(phrase)) {
+            return true;
+        } else if (where.equals("start") && title.substring(0, title.length()/2).contains(phrase)) {
+            return true;
+        } else if (where.equals("end") && title.substring(title.length()/2).contains(phrase)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
